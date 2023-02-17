23fbc367dbc52f922ac56f46089f12cb64927b26
Rubel Hossain <rubel8525@yahoo.com>
2023-02-18 01:28:57 +0600

    added some simple math

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hellow Golang")
+	fmt.Println("5+4 = ", 5+4)
+	fmt.Println("10 - 4 = ", 10-4)
+	fmt.Println("5*4 = ", 5*4)
+	fmt.Println("20/5 = ", 20/5)
 }
